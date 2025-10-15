babf55f8dde82240ebd3ab9f251863bfe3c60f49
manyamOpenthrive <manyam.c@openthrive.com>
2025-10-15 16:12:10 +0530

    few

--- medusa-store/medusa-config.js
+++ medusa-store/medusa-config.js
@@ -1,4 +1,4 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+const { loadEnv, defineConfig } = require('@medusajs/framework/utils')
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
@@ -6,12 +6,12 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
   modules: [
     {
@@ -21,13 +21,12 @@ module.exports = defineConfig({
         project_id: process.env.SANITY_PROJECT_ID,
         api_version: new Date().toISOString().split("T")[0],
         dataset: "production",
-        studio_url: process.env.SANITY_STUDIO_URL ||
-          "http://localhost:3000/studio",
+        studio_url:
+          process.env.SANITY_STUDIO_URL || "http://localhost:3000/studio",
         type_map: {
           product: "product",
         },
       },
     },
   ],
-
 })
